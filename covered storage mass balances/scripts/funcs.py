122c4dd6e7687cd1cc4fc846939e06458fc6d303
Isaiah Guenther <izguenther6@gmail.com>
2024-04-12 21:37:40 -0400

    castile updates
    tracked total volume for year 2023, total pumping cost, added snow

--- covered storage mass balances/scripts/funcs.py
+++ covered storage mass balances/scripts/funcs.py
@@ -10,20 +10,32 @@ def convert_units(df):
     '''
     need correct units to use equation from paper
     -
-    returns | dfconvert: dataframe of converted values with precipitation in mm and temp in C
-              aconvert: converted area [mm^2]
+    returns | df: dataframe of converted values with precipitation, snowfall in mm and temp in C
     -
     args | df: original dataframe 
            area: area of covered storage [ft^2]
     '''
+    #get rid of trace (T) and missing (M) values as zeros...also remove (S), not sure what this is
+    df = df.replace(to_replace='T', value = 0.0)
+    df = df.replace(to_replace='M', value = 0.0)
+    df = df.replace(to_replace='S', value = 0.0)
+    
+    #make sure all values are numeric...helps catch errors in data entries
+    pd.to_numeric(df['Tavg [F]'], errors='coerce')
+    pd.to_numeric(df['P [in]'])
+    pd.to_numeric(df['S [in]'])
+    pd.to_numeric(df['Rhavg'])
 
-    dfconvert = df.copy()
-    dfconvert['precip [in]'] = df.loc[:,'precip [in]'] * 25.4 #1 inch = 25.4 mm
-    dfconvert['Tavg [F]'] = (df.loc[:,'Tavg [F]'] - 32) * 5/9 #F to C
-    dfconvert = dfconvert.rename(columns={'precip [in]' : 'P[mm]', 'Tavg [F]': 'Tavg[C]'})
-    dfconvert = dfconvert.round(2) #round decimal places
+    #convert P, S, and T
+    df['P [in]'] = df.loc[:,'P [in]'] * 25.4 #1 inch = 25.4 mm
+    df['S [in]'] = df.loc[:,'S [in]'] * 25.4 #1 inch = 25.4 mm
+    df['Tavg [F]'] = (df.loc[:,'Tavg [F]'] - 32) * 5/9 #F to C
 
-    return dfconvert
+    #rename columns
+    df = df.rename(columns={'P [in]' : 'P[mm]', 'S [in]': 'S[mm]', 'Tavg [F]': 'Tavg[C]'})
+    df = df.round(2) #round decimal places
+
+    return df
 
 
 def mass_bal(df, area):
@@ -37,25 +49,43 @@ def mass_bal(df, area):
     
     days = df.shape[0]
     wks = int((days - days % 7) / 7) #only looking at full weeks
-    dfmb = pd.DataFrame(columns=['week', 'Pin[mm]','Tavg[C]', 'Rhavg[%]', 'Eout[mm]', 'Net Change[ft^3]', 'Pumping Cost [$]'])
+    dfmb = pd.DataFrame(columns=['week', 'Tavg[C]', 'P[mm]', 'S[mm]', 'Rhavg[%]', 'Eout[mm]', 'Net Volume Change[ft^3]', 
+                                 'Total Vol [ft^3]', 'Pumping Cost Change [$]', 'Pumping Cost Total [$]'], index = range(1, wks + 1))
+#    dfmb.at[0, 'Total Vol [ft^3]'] = 0 #set initial volume to 0
 
-    for i in range(wks + 1):
-        wk = i + 1 #week
+    for wk in range(1, wks + 1):
         end_date = wk * 7 #end date index for week
         start_date = end_date - 7 #start date index for week
 
-        #perform mass balance: (Pin [mm/wk] - Eout [mm/wk]) * area [ft^2] * 1 [wk] = net [ft^3]
-        Pin = df.iloc[start_date:end_date, 1].sum() #cumulative precipitation for wk [mm/wk]
-        avgs = df.iloc[start_date:end_date, 2:].mean() #weekly averages for equation
-        Eout = 10 * (0.135 * avgs[0] - 0.024 * Pin - 0.099 * avgs[1] + 7.14) #equation from paper for raw manure evaporation, converted to [mm/wk]
-        net = (Pin - Eout) * 0.00328 * area #net water volume change of system [ft^3]
+        #perform mass balance: (P[mm/wk] + S[mm] - Eout [mm/wk]) * area [ft^2] * 1 [wk] = chg [ft^3]
+        #totV = prev + net
+        #totPC = prev + pcost
+        P = df.iloc[start_date:end_date, 2].sum() #cumulative precipitation for wk [mm/wk]
+        Sconv = .1 #estimate for water content in snow
+        S = df.iloc[start_date:end_date, 3].sum() * Sconv #cumulative snowfall for wk [mm/wk]
+        Ptot = P + S
+        Tavg = df.iloc[start_date:end_date, 1].mean() #weekly average for temperature [C]
+        Rhavg = df.iloc[start_date:end_date, 4].mean() #weekly average for temperature
+        Eout = 10 * (0.135 * Tavg - 0.024 * Ptot - 0.099 * Rhavg + 7.14) #equation from paper for raw manure evaporation, converted to [mm/wk]
+        if Eout < 0: Eout = 0
+        net = (Ptot - Eout) * 0.00328 * area #net water volume change of system [ft^3]
+        if net > 0: pcost = net * 7.481 * 0.01 #financial calculation: pumping cost = net[ft^3] * 7.481 [gal/ft^3] * 0.01 [$/gal]
+        else: pcost = 0
+        if wk == 1:  #assume starting at zero
+            totV = net
+            totPC = pcost  
+        else: 
+            totV = dfmb.loc[wk - 1, 'Total Vol [ft^3]'] + net #total vol of additional water in storage
+            totPC = dfmb.loc[wk - 1, 'Pumping Cost Total [$]'] + pcost #total vol of additional water in storage
 
-        #financial calculation: pumping cost = net[ft^3] * 7.481 [gal/ft^3] * 0.01 [$/gal]
-        pcost = net * 7.481 * 0.01
+        
 
-        row = pd.Series([wk, Pin, avgs[0], avgs[1], Eout, net, pcost], index= ['week', 'Pin[mm]', 'Tavg[C]', 'Rhavg[%]', 'Eout[mm]', 'Net Change[ft^3]', 'Pumping Cost [$]']) #row to add to dfmb
-        dfmb.loc[wk] = row
-        dfmb= dfmb.round(2)
+        row = pd.Series([wk, Tavg, P, S, Rhavg, Eout, net, totV, pcost, totPC], 
+                        index= ['week', 'Tavg[C]', 'P[mm]', 'S[mm]', 'Rhavg[%]', 'Eout[mm]', 'Net Volume Change[ft^3]', 
+                                 'Total Vol [ft^3]', 'Pumping Cost Change [$]', 'Pumping Cost Total [$]']) #row to add to dfmb
+        dfmb.loc[wk] = row.round(2)
+
+    dfmb= dfmb.round(2)
     
     return dfmb
 
